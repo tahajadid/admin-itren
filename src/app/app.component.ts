5c4a99a4b13c224dd8ab14a0a15282cb54443006
taha jadid <tahajadid98@gmail.com>
2022-02-19 19:42:54 +0100

    add config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,12 @@ import { RestService } from './services/rest.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
+
   title = 'admin-itsren';
  
-  customers=[];
+  customers:customer[] = []
   status:any;
   constructor(private restService:RestService,private router:Router ){
 
@@ -18,19 +20,28 @@ export class AppComponent {
   getAll(){
     // GET Request to get all customers
     this.restService.getAllCustomers().subscribe((data:any)=>{
-      this.customers=data.data
+      this.customers=data.listOfCustomers
       this.status=data.status;
     },error=>{
       this.status=0
       console.log("status is after error ",this.status)     })
   }
-  getCustomerByID(id:String|number){
+  getCustomerByID(id:any){
     // GET Request to get all customers
     this.restService.getListOfCustomers(id).subscribe((data:any)=>{
-      this.customers=data.data
+      this.customers=data.listOfCustomers
       this.status=data.status;
     },error=>{
       this.status=0
       console.log("status is after error ",this.status)     })
   }
 }
+
+export interface customer{
+  id_person: number,
+  full_name: string,
+  email: string,
+  phone: string,
+  id_training: string
+};
+
--- src/app/services/rest.service.ts
+++ src/app/services/rest.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-const baseUrlListOfCustomers='https://admin.itsren-academy.com/api/listOfCustomers';
+const baseUrlListOfCustomers='http://admin.itsren-academy.com/api/listOfCustomers';
 const headers = { 'content-type': 'application/json'} 
 @Injectable()
 export class RestService {
 
   constructor(private http: HttpClient) { }
-  getListOfCustomers(id) {
+  getListOfCustomers(id:any) {
     return this.http.get(`${baseUrlListOfCustomers}/${id}`,{
       headers: new HttpHeaders().set('authorization', 'rachuser@1998'),
     });
